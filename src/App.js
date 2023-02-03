d7fd515f18edc54c779c52aa4ad3e9a58551a7ee
Drew Coker <drewrosecoker@gmail.com>
2023-02-03 12:19:19 -0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header><div className="header-content">
+      <Navbar bg="light" expand="lg">
+      <Container>
+      <img src="/assets/pizzalogo.png"/>
+        <Navbar.Brand href="#home">Vic's Pizza</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Food</Nav.Link>
+            <Nav.Link href="#link">Drinks</Nav.Link>
+            <Nav.Link href="#link">Dessert</Nav.Link>
+            <Nav.Link href="#link">Order Online</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      </div>
+    </header>
+   
+
   );
 }
 
